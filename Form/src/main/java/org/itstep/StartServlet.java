0cdec890f946d67554744e9678f2058b817c86ea
hp <hp@172.21.213.161>
2018-08-28 20:37:18 +0300

    Commit

--- Form/src/main/java/org/itstep/StartServlet.java
+++ Form/src/main/java/org/itstep/StartServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/")
 public class StartServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static String pages = "/WEB-INF/pages/myPages.html";
+	private static String pages = "/WEB-INF/pages/MyPages.html";
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher(pages).forward(request, response);
